a98f933a2f4c0e130ef807eda0790c80823588d4
Bastian Benitez <92552627+BastianBenitez@users.noreply.github.com>
2025-08-24 16:52:52 -0400

    Remove all project files
    Deleted all source, configuration, and asset files from the repository, including frontend, backend, and build-related files. This effectively removes the entire project.

--- src-tauri/src/command_ejemplo.rs
+++
@@ -1,6 +0,0 @@
-// Aqui habra un ejemplo de comando de rust fuera de lib.rs
-#[tauri::command]
-pub fn ej_command_externo(name: &str) -> String{
-    format!("Mi destino es ser llamado por react {}, estoy fuera de lib.rs",name)
-}
-// Ahora hay que agregarte en lib.rs
--- src/components/views/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./DashboardView";
-export * from "./LifecycleView";
-export * from "./AnalyticsView";
-export * from "./ProjectsView";
-export * from "./TeamView";
-export * from "./SettingsView";
-export * from "./HelpView";
